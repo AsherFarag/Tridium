5b943979e07854402f43e1441918f4844d755983
Asher Farag <AsherFarag@hotmail.com>
2024-05-13 16:34:29 +1000

    Added skeleton Delegate Class

--- Tridium/source/Tridium/Events/Event.h
+++ Tridium/source/Tridium/Events/Event.h
@@ -5,7 +5,7 @@
 namespace Tridium {
 
 	// Events in Tridium are currently blocking, meaning when an event occurs it
-	// immediately gets dispatched and must be dealt with right then an there.
+	// immediately gets dispatched and must be dealt with right then and there.
 	// For the future, a better strategy might be to buffer events in an event
 	// bus and process them during the "event" part of the update stage.
 
